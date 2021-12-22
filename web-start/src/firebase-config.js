c832ce26ee6c924d4f1734fc5bf625b8641cd2b9
Majid Kareem <kareemmajid86@gmail.com>
2021-12-22 18:11:50 +0100

    Complete chat application
    Add firestore to persist chat data and google storage to save chat images

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -1,6 +1,6 @@
 /**
  * To find your Firebase config object:
- * 
+ *
  * 1. Go to your [Project settings in the Firebase console](https://console.firebase.google.com/project/_/settings/general/)
  * 2. In the "Your apps" card, select the nickname of the app for which you need a config object.
  * 3. Select Config from the Firebase SDK snippet pane.
@@ -8,12 +8,21 @@
  */
 const config = {
   /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: 'AIzaSyDaEMO471j9dJO2joAmNfmjT6RvF8_epnI',
+  authDomain: 'friendlychat-bae0f.firebaseapp.com',
+  projectId: 'friendlychat-bae0f',
+  storageBucket: 'friendlychat-bae0f.appspot.com',
+  messagingSenderId: '784355716455',
+  appId: '1:784355716455:web:b75a8193306b2f539b13c2',
 };
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
-    throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
+    throw new Error(
+      'No Firebase configuration object provided.' +
+        '\n' +
+        "Add your web app's configuration object to firebase-config.js"
+    );
   } else {
     return config;
   }
